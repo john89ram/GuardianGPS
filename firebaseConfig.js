0cd16c46f62a5b9e8d09dd351998ef370bfb43ba
Jonathan Ramirez <john89ram@gmail.com>
2025-04-10 11:12:13 -0500

    .

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,11 +1,10 @@
 // firebaseConfig.js
 export const firebaseConfig = {
-    apiKey: "AIzaSyAEt8vZq-glh1shei1p24iOd5I5NWgBq4o",
-    authDomain: "guardiangpshive.firebaseapp.com",
-    projectId: "guardiangpshive",
-    storageBucket: "guardiangpshive.appspot.com",
-    messagingSenderId: "262885831294",
-    appId: "1:262885831294:web:22b65b0eda06c74b749e2f",
-    measurementId: "G-TVRMJC6YDB"
+    apiKey: "AIzaSyC-Eqoruor79SJI6d-adsFD2FlpYocUk18",
+    authDomain: "guardiangps-2d5d3.firebaseapp.com",
+    projectId: "guardiangps-2d5d3",
+    storageBucket: "guardiangps-2d5d3.appspot.com",
+    messagingSenderId: "1041236876427",
+    appId: "your-app-id", // You can leave blank or paste full ID if needed
   };
   
