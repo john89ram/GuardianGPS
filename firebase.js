0ac4ad633bb7dbfb26bd94033a26e61637c4c48a
Jonathan Ramirez <john89ram@gmail.com>
2025-04-10 15:20:54 -0500

    Restart.

--- firebase.js
+++
@@ -1,7 +0,0 @@
-// firebase.js
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-import { firebaseConfig } from "./firebaseConfig.js";
-
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
--- kid/emailjs_config.js
+++
@@ -1,4 +0,0 @@
-(function() {
-    emailjs.init("XZpDjjUIyCedr-e4n"); // Your EmailJS Public Key
-  })();
-  
