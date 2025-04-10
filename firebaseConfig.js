23a0f453c584b26c6df36ad44e07415036b89d10
Jonathan Ramirez <john89ram@gmail.com>
2025-04-10 11:03:39 -0500

    .

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyAEt8vZq-glh1shei1p24iOd5I5NWgBq4o",
+    authDomain: "guardiangpshive.firebaseapp.com",
+    projectId: "guardiangpshive",
+    storageBucket: "guardiangpshive.appspot.com",
+    messagingSenderId: "262885831294",
+    appId: "1:262885831294:web:22b65b0eda06c74b749e2f",
+    measurementId: "G-TVRMJC6YDB"
+  };
+  
