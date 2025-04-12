f027967cd2764cc19fc0b01ada3bda2efcf28f32
Jonathan Ramirez <john89ram@gmail.com>
2025-04-12 14:47:28 -0500

    Removal of GoogleAPI key from front-end.

--- kid/config.js
+++ kid/config.js
@@ -1,5 +1,5 @@
+// config.js for Kid Portal
 const EMAILJS_USER_ID = "XZpDjjUIyCedr-e4n";
 const EMAILJS_SERVICE_ID = "service_017si3q";
 const EMAILJS_TEMPLATE_ID = "template_j2j5ij8";
 const PARENT_EMAIL = "john89ram@gmail.com";
-const GOOGLE_ELEVATION_API_KEY = "AIzaSyDVuxDfESQFawetEJdkaae36dZcrDEle-w";
--- parent/config.js
+++ parent/config.js
@@ -1,9 +1,8 @@
+// config.js for Parent Portal
 const EMAILJS_USER_ID = "XZpDjjUIyCedr-e4n";
 const EMAILJS_SERVICE_ID = "service_017si3q";
 const EMAILJS_TEMPLATE_ID = "template_j2j5ij8";
 const PARENT_EMAIL = "john89ram@gmail.com";
-const GOOGLE_ELEVATION_API_KEY = "AIzaSyDVuxDfESQFawetEJdkaae36dZcrDEle-w";
-
 
 
 // -----------------------------------------------------------
