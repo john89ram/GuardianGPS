7f9b420ddd99d1a655a2579a60e8c805d9aeeba4
Jonathan Ramirez <john89ram@gmail.com>
2025-04-14 12:35:18 -0500

    V2 deployment

--- kid/config.js
+++
@@ -1,5 +0,0 @@
-// config.js for Kid Portal
-const EMAILJS_USER_ID = "XZpDjjUIyCedr-e4n";
-const EMAILJS_SERVICE_ID = "service_017si3q";
-const EMAILJS_TEMPLATE_ID = "template_j2j5ij8";
-const PARENT_EMAIL = "john89ram@gmail.com";
